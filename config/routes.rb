c797b5b3a3119ae359ee7a512bfccb763a6062a7
Emil Kampp <emil@kampp.me>
2012-05-05 21:41:41 +0200

    Refactoring mailer translations

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ FutureGame::Application.routes.draw do
   # Maps the user account related paths
   resource :users, path: "account"
   match 'my_account', to: "users#edit", as: :my_account
+
+  # Maps login session paths
+  resource :sessions, only: [ :new, :create, :destroy ]
   match 'login', to: "sessions#new", as: :login
   match 'logout', to: "sessions#destroy", as: :logout
-  resource :sessions, only: [ :new, :create, :destroy ]
   match 'auth/developer/callback', to: "sessions#create"
 
   root :to => 'home#index'
